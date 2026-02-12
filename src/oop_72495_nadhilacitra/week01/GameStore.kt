feed62b9671b35d063ac2080ec31b848678e9d47
dhilacitra <dhilacitra6@gmail.com>
2026-02-12 19:33:31 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_72495_nadhilacitra/week01/GameStore.kt
+++ src/oop_72495_nadhilacitra/week01/GameStore.kt
@@ -1,5 +1,8 @@
 package oop_72495_nadhilacitra.week01
 
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) (price * 20) / 100
+    else (price * 10) / 100
 
 fun main() {
     val gameTitle = "Warnet Simulator"
