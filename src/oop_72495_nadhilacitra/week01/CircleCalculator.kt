21670966e01375922433abb43900b681ba074b33
dhilacitra <dhilacitra6@gmail.com>
2026-02-12 19:20:20 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_72495_nadhilacitra/week01/CircleCalculator.kt
+++ src/oop_72495_nadhilacitra/week01/CircleCalculator.kt
@@ -4,7 +4,7 @@ fun checkSize(area: Double) =
     if (area > 100) "This is a Big Circle"
     else "This is a Small Circle"
 
-fun main(args: Array<String>) {
+fun main() {
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
