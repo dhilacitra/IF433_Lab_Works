468cc7de397841319e958f2fd127e985b21a00b1
dhilacitra <dhilacitra6@gmail.com>
2026-02-12 18:30:09 +0700

    week01: add calculateStatus function

--- src/oop_72495_nadhilacitra/week01/GradingSystem.kt
+++ src/oop_72495_nadhilacitra/week01/GradingSystem.kt
@@ -1,17 +1,15 @@
 package oop_72495_nadhilacitra.week01
 
+// Tulis DI LUAR main()
+fun calculateStatus(score: Int) =
+    if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
     val name = "John Doe"
     val score = 80
 
     println("Nama: $name, Nilai: $score")
 
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
-    }
-
-    println("Grade kamu: $grade")
+    // Panggil DI DALAM main()
+    println("Status: ${calculateStatus(score)}")
 }
