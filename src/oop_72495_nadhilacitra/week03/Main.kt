36a11c1f5f56a9094dd412112f23052cbffaef53
dhilacitra <dhilacitra6@gmail.com>
2026-02-20 15:25:01 +0700

    week03: cleanup code

--- src/oop_72495_nadhilacitra/week03/Main.kt
+++ src/oop_72495_nadhilacitra/week03/Main.kt
@@ -3,22 +3,8 @@ package oop_72495_nadhilacitra.week03
 fun main(){
 
     val e = Employee("Budi")
-
-    println("--- Test Validasi Salary ---")
-    e.salary = -1000      // Harus print Error
     e.salary = 5000000
-    println("Gaji: ${e.salary}")
-
-    println()
-
-    println("--- Test Encapsulation ---")
     e.increasePerformance()
-
-
     e.printStatus()
-
-    println()
-
-    println("--- Test Computed Property ---")
-    println("Pajak yang harus dibayar: ${e.tax}")
+    println("Pajak: ${e.tax}")
 }
