462c681c84a7cfb8663da111ca957ac7732e94f0
dhilacitra <dhilacitra6@gmail.com>
2026-02-20 15:30:06 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_72495_nadhilacitra/week03/Main.kt
+++ src/oop_72495_nadhilacitra/week03/Main.kt
@@ -2,9 +2,12 @@ package oop_72495_nadhilacitra.week03
 
 fun main(){
 
-    val e = Employee("Budi")
-    e.salary = 5000000
-    e.increasePerformance()
-    e.printStatus()
-    println("Pajak: ${e.tax}")
+    val weapon = Weapon("Dragon Slayer", 500)
+
+    weapon.damage = -50
+    weapon.damage = 9999
+
+    println("Nama Weapon: ${weapon.name}")
+    println("Damage: ${weapon.damage}")
+    println("Tier: ${weapon.tier}")
 }
