e3e439ef798f5aadc3b90256aa1ccc0d478517c2
dhilacitra <dhilacitra6@gmail.com>
2026-02-20 15:13:39 +0700

    week03: add private property

--- src/oop_72495_nadhilacitra/week03/Employee.kt
+++ src/oop_72495_nadhilacitra/week03/Employee.kt
@@ -1,8 +1,9 @@
 package oop_72495_nadhilacitra.week03
 
 class Employee(val name: String){
+
     var salary: Int = 0
-        set (value){
+        set(value){
             if (value < 0){
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
                 field = 0
@@ -10,4 +11,20 @@ class Employee(val name: String){
                 field = value
             }
         }
+
+    private var performanceRating: Int = 3
+
+    fun increasePerformance(){
+        if (performanceRating < 5){
+            performanceRating++
+            println("Kinerja $name meningkat! Rating: $performanceRating")
+        } else {
+            println("Rating $name sudah maksimal!")
+        }
+    }
+
+    fun printStatus(){
+        println("Karyawan: $name")
+        println("Rating: $performanceRating")
+    }
 }
