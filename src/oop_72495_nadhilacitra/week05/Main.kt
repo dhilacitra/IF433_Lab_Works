57be08df2f2574d26220c8f6692a220042f78581
dhilacitra <dhilacitra6@gmail.com>
2026-03-06 15:02:54 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_72495_nadhilacitra/week05/Main.kt
+++ src/oop_72495_nadhilacitra/week05/Main.kt
@@ -2,14 +2,14 @@ package oop_72495_nadhilacitra.week05
 
 fun main() {
 
-    val mathHelper = MathHelper()
+    val eWallet = EWallet("Budi", 50000.0)
+    val creditCard = CreditCard("Andi", 100000.0)
 
-    val luasPersegi = mathHelper.hitungLuas(4)
-    println("Luas Persegi: $luasPersegi")
+    val paymentMethods: List<PaymentMethod> = listOf(eWallet, creditCard)
 
-    val luasPersegiPanjang = mathHelper.hitungLuas(5, 3)
-    println("Luas Persegi Panjang: $luasPersegiPanjang")
-
-    val luasLingkaran = mathHelper.hitungLuas(7.0)
-    println("Luas Lingkaran: $luasLingkaran")
+    for (method in paymentMethods) {
+        println("Memproses pembayaran untuk ${method.accountName}")
+        method.processPayment(75000.0)
+        println()
+    }
 }
