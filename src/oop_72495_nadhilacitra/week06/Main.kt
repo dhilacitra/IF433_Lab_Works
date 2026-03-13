094aad1e31a665cc08f988d9bf0f0f20d78a1247
dhilacitra <dhilacitra6@gmail.com>
2026-03-13 15:29:00 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_72495_nadhilacitra/week06/Main.kt
+++ src/oop_72495_nadhilacitra/week06/Main.kt
@@ -1,22 +1,9 @@
 package oop_72495_nadhilacitra.week06
 
-fun processCheckout(method: PaymentMethod, amount: Double) {
-    println("-> Memulai checkout...")
-    method.pay(amount)
-}
+fun main() {
 
-fun main(){
-    val myWatch = Smartwatch()
-    myWatch.showtime()
-
-    val myPhone = Smartphone()
-    myPhone.turnOn()
-
-    val pay1 = Gopay()
-    val pay2 = CreditCard()
-
-    println("\n=== TESTING CHECKOUT ===")
-    processCheckout(method = pay1, amount = 50000.0)
-    processCheckout(method = pay2, amount = 150000.0)
+    val lamp = SmartLamp("L01", "Ruang Tamu")
+    val speaker = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctv = SmartCCTV("C01", "Ezviz Garasi")
 
 }
