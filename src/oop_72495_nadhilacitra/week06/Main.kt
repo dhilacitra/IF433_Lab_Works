9b361c1144191eb59cb547f72a5da902f0f721af
dhilacitra <dhilacitra6@gmail.com>
2026-03-13 15:30:09 +0700

    week06: (task) test smart home system logic in main

--- src/oop_72495_nadhilacitra/week06/Main.kt
+++ src/oop_72495_nadhilacitra/week06/Main.kt
@@ -2,8 +2,20 @@ package oop_72495_nadhilacitra.week06
 
 fun main() {
 
+    // Instansiasi perangkat
     val lamp = SmartLamp("L01", "Ruang Tamu")
     val speaker = SmartSpeaker("S01", "Google Nest Dapur")
     val cctv = SmartCCTV("C01", "Ezviz Garasi")
 
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("\n=== Mengaktifkan Security Mode ===")
+    hub.activateSecurityMode()
+
+    println("\n=== Mematikan Semua Perangkat Switchable ===")
+    hub.turnOffAllSwitches()
 }
