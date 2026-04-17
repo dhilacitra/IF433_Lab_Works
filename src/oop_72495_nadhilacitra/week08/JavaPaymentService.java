a1b9abde9b81d382602f51d74289739c18872b2d
dhilacitra <dhilacitra6@gmail.com>
2026-04-17 16:08:11 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_72495_nadhilacitra/week08/JavaPaymentService.java
@@ -0,0 +1,9 @@
+package oop_72495_nadhilacitra.week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+
+}
