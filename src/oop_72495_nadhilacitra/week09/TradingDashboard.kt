b3110de1c112f585db97d127068d2c2b06ebb2ea
dhilacitra <dhilacitra6@gmail.com>
2026-04-24 15:30:38 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_72495_nadhilacitra/week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_72495_nadhilacitra.week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, 8.7, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 25, -12.4, "CLOSED"),
+        TradeLog("XRPUSDT", "LONG", 5, 6.3, "OPEN"),
+        TradeLog("BNBUSDT", "SHORT", 8, -3.1, "OPEN")
+    )
+
+}
