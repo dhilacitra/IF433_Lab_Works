687019829e0dccfe99f6ade86f3d3db01b573702
dhilacitra <dhilacitra6@gmail.com>
2026-05-10 20:03:59 +0700

    week10: (task) implement search logic in repository

--- src/oop_72495_nadhilacitra/week10/WalletModels.kt
+++ src/oop_72495_nadhilacitra/week10/WalletModels.kt
@@ -1,9 +1,13 @@
 package oop_72495_nadhilacitra.week10
 
+interface HasName {
+    val name: String
+}
+
 data class Coin(
-    val name: String,
+    override val name: String,
     val balance: Double
-)
+) : HasName
 
 data class Transaction(
     val id: String,
