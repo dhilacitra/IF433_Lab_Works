899aef6a4d4a2993b742dd140e5740ef48c19df8
dhilacitra <dhilacitra6@gmail.com>
2026-05-10 19:48:14 +0700

    week10: implement where clause for comparable constraints

--- src/oop_72495_nadhilacitra/week10/NumericalStorage.kt
+++ src/oop_72495_nadhilacitra/week10/NumericalStorage.kt
@@ -1,7 +1,11 @@
 package oop_72495_nadhilacitra.week10
 
-class MathBox<T : Number>(val value1: T, val value2: T){
-    fun sum(): Double{
+class MathBox<T : Number>(val value1: T, val value2: T) {
+    fun sum(): Double {
         return value1.toDouble() + value2.toDouble()
     }
+    fun<T> getMax(a: T, b: T): T where T: Comparable<T>{
+        return if(a>b) a else b
+    }
 }
+
